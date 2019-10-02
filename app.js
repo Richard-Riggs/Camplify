93b2a80016d02c26102bef63c2a55109600488c9
EC2 Default User <ec2-user@ip-172-31-34-239.ec2.internal>
2019-10-02 15:10:18 +0000

    add styling to campground page and new Campground form

--- app.js
+++ app.js
@@ -3,13 +3,19 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 
-const hostname = '127.0.0.1';
-const port = 3000;
+
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 var campgrounds = [
+    {name: "Salmon Creek", image:"https://cdn.pixabay.com/photo/2016/11/22/23/08/adventure-1851092_960_720.jpg"},
+    {name: "Granite Hill", image:"https://cdn.pixabay.com/photo/2018/12/24/22/19/camping-3893587_960_720.jpg"},
+    {name: "Mountain Goat's Rest", image:"https://cdn.pixabay.com/photo/2016/02/18/22/16/tent-1208201_960_720.jpg"},
+    {name: "Salmon Creek", image:"https://cdn.pixabay.com/photo/2016/11/22/23/08/adventure-1851092_960_720.jpg"},
+    {name: "Granite Hill", image:"https://cdn.pixabay.com/photo/2018/12/24/22/19/camping-3893587_960_720.jpg"},
+    {name: "Mountain Goat's Rest", image:"https://cdn.pixabay.com/photo/2016/02/18/22/16/tent-1208201_960_720.jpg"},
     {name: "Salmon Creek", image:"https://cdn.pixabay.com/photo/2016/11/22/23/08/adventure-1851092_960_720.jpg"},
     {name: "Granite Hill", image:"https://cdn.pixabay.com/photo/2018/12/24/22/19/camping-3893587_960_720.jpg"},
     {name: "Mountain Goat's Rest", image:"https://cdn.pixabay.com/photo/2016/02/18/22/16/tent-1208201_960_720.jpg"}
@@ -37,6 +43,15 @@ app.get("/campgrounds/new", function (req, res) {
 });
 
 //================== START SERVER ======================
+
+//          LOCAL ENVIRONMENT
+// const hostname = '127.0.0.1';
+// const port = 3000;
+
+
+//          AWS Cloud9 ENVIRONMENT
+const hostname = '0.0.0.0'
+const port = 8080;
 
 app.listen(port, hostname, function () {
     console.log("The YelpCamp server has started")
