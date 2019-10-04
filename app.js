fad443fa8f40967287787ff7dddaf83b71ba2d46
EC2 Default User <ec2-user@ip-172-31-34-239.ec2.internal>
2019-10-04 21:27:26 +0000

    add MongoDB Atlas connectivity

--- app.js
+++ app.js
@@ -1,53 +1,87 @@
-//===================== PACKAGES ========================
-const express = require("express");
-const app = express();
+//======================= PACKAGES =======================
+
+const express    = require("express");
 const bodyParser = require("body-parser");
+const mongoose   = require("mongoose");
 
 
+//=================== EXPRESS CONFIG ====================
 
+const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
-var campgrounds = [
-    {name: "Salmon Creek", image:"https://cdn.pixabay.com/photo/2016/11/22/23/08/adventure-1851092_960_720.jpg"},
-    {name: "Granite Hill", image:"https://cdn.pixabay.com/photo/2018/12/24/22/19/camping-3893587_960_720.jpg"},
-    {name: "Mountain Goat's Rest", image:"https://cdn.pixabay.com/photo/2016/02/18/22/16/tent-1208201_960_720.jpg"},
-    {name: "Salmon Creek", image:"https://cdn.pixabay.com/photo/2016/11/22/23/08/adventure-1851092_960_720.jpg"},
-    {name: "Granite Hill", image:"https://cdn.pixabay.com/photo/2018/12/24/22/19/camping-3893587_960_720.jpg"},
-    {name: "Mountain Goat's Rest", image:"https://cdn.pixabay.com/photo/2016/02/18/22/16/tent-1208201_960_720.jpg"},
-    {name: "Salmon Creek", image:"https://cdn.pixabay.com/photo/2016/11/22/23/08/adventure-1851092_960_720.jpg"},
-    {name: "Granite Hill", image:"https://cdn.pixabay.com/photo/2018/12/24/22/19/camping-3893587_960_720.jpg"},
-    {name: "Mountain Goat's Rest", image:"https://cdn.pixabay.com/photo/2016/02/18/22/16/tent-1208201_960_720.jpg"}
-];
+
+//======================= DATABASE =======================
+
+// enter the following arguments when starting app.js:
+// $ node app.js --username=<atlas username> --password=<atlas password>
+
+//Connect to MongoDB Atlas
+const atlasUsername = process.argv[2].split("=")[1];
+const atlasPassword = process.argv[3].split("=")[1];
+const connectionString = `mongodb+srv://${atlasUsername}:${atlasPassword}@cluster0-pekd8.mongodb.net/yelpCamp?retryWrites=true&w=majority`;
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect(connectionString, { useNewUrlParser: true });
+
+
+//Campground schema & model
+const campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+const Campground = mongoose.model("Campground", campgroundSchema);
+
 
 //===================== ROUTES =========================
 
-app.get("/", function (req, res) {
-    res.render("landing");
-});
+app.route('/')
+    .get((req, res) => {
+        res.render("landing");
+    });
 
-app.get("/campgrounds", function (req, res) {
-    res.render("campgrounds", {campgrounds: campgrounds});
-});
+app.route('/campgrounds')
+    .get((req, res) => {
+        Campground.find({}, (error, campgrounds) => {
+            if (error) {
+                console.log(error);
+            }
+            else {
+                res.render("campgrounds", {campgrounds: campgrounds});
+            }
+        })
+    })
+    .post((req, res) => {
+        Campground.create(
+            {
+                name: req.body.name,
+                image: req.body.image
+                
+            }, function(error, campground) {
+                if (error) {
+                    console.log(error);
+                }
+                else {
+                    console.log("Created new campground document:");
+                    console.log(campground);
+                }
+            });
+        res.redirect("/campgrounds");        
+    });
 
-//Push new campground object to campgrounds array
-//Redirect to campgrounds page
-app.post("/campgrounds", function (req, res) {
-    campgrounds.push({name: req.body.name, image: req.body.image});
-    res.redirect("/campgrounds")
-});
 
-app.get("/campgrounds/new", function (req, res) {
-    res.render("new")
-});
+app.route('/campground/new')
+    .get((req, res) => {
+        res.render("new")
+    });
 
 //================== START SERVER ======================
 
 //          LOCAL ENVIRONMENT
 // const hostname = '127.0.0.1';
 // const port = 3000;
-
+// Access local website here:   http://127.0.0.1:3000/
 
 //          AWS Cloud9 ENVIRONMENT
 const hostname = '0.0.0.0'
@@ -57,4 +91,4 @@ app.listen(port, hostname, function () {
     console.log("The YelpCamp server has started")
 });
 
-// Access website at 'http://127.0.0.1:3000/'
+//Access Cloud9 website here:   https://63dcbade7414481f8c2640d0eca49682.vfs.cloud9.us-east-1.amazonaws.com/
