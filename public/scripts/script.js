092ff96e273d2afcef466636539eef84be45ab4c
EC2 Default User <ec2-user@ip-172-31-34-239.ec2.internal>
2020-01-15 23:49:23 +0000

    introduced styling for new favorite button

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,5 +1,10 @@
 $(function () {
-
     // All scripts go inside this function
+    $('.favorite-form').submit(function(event) {
+        event.preventDefault();
+        let $form = $(this),
+            url = $form.attr( "action" );
+        alert("Would post " + url);
+    });
 
 });
